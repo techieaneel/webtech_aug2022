ef701ee1dfb5e980c43cc7a6ff55640821c0faff
Aneel Thirumalaraju <techieaneel@gmail.com>
2023-06-02 07:46:56 +0530

    installing bootstrap and fontAwesome in React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 // if component export without default - named import
 // import {Hone} from './Hone';
@@ -22,14 +24,48 @@ import './App.css';
 function App() {
   return (
     <React.Fragment>
-      <div className="container">
-        <div className="header">
-          Hello Changed Text
+      <div class="jumbotron text-center">
+        <h1>My First Bootstrap Page</h1>
+        <p>Resize this responsive page to see the effect!</p>
+        {/*<i class="fa fa-user"></i>*/}
+        {/* <FontAwesomeIcon icon="fa fa-user" /> */}
+        <FontAwesomeIcon icon={faUser} />
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <h3>Column 1</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+          <div class="col-sm-4">
+            <h3>Column 2</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+          <div class="col-sm-4">
+            <h3>Column 3</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
         </div>
       </div>
-      <div className="footer"></div>
     </React.Fragment>
   );
 }
 
 export default App;
+
+// default import and export
+// module OR named import and export
+
+// sending our (local) changes to git repository (online)
+// git status
+// git add
+// git commit
+// git push
+
+// Receiving our git repository (online) code to local
+// git fetch
+// git pull
+
+// git clone
+// git init
